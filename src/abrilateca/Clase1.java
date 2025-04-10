400ff1d61ed9dc872867ba5fe5c01dcaede6f127
dam <dam@203P25>
2025-04-10 11:25:53 +0200

    Commit 1, etiqueta version 1

---
+++ src/abrilateca/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilateca;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    public void nombreApellido(){
+        String nombre = "Guillermo";
+        String apellidos = "Ateca Baranda";
+    }
+}
