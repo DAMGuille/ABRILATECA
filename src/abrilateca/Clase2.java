ea03d7e3611d2c2e738f75edb237751dff1edf09
dam <dam@203P25>
2025-04-10 11:38:24 +0200

    Commit 2, Rama Produccion

---
+++ src/abrilateca/Clase2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilateca;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    private String produc;
+    private String nombreApe;
+    
+    public void produc(){
+        produc = "esto es PRODUCCION";
+        nombreApe= "GuillermoAtecaBaranda";
+    }
+}
